c0d46c7c71bf6a662e38a20bbd71814989e4c6c4
Kassia-Hikari <kassiahikari@outlook.com>
2025-03-22 17:58:20 -0400

    Add files via upload
    Atividade aula 2 (18/03/2025)

---
+++ atividade.py
@@ -0,0 +1,57 @@
+# # programa da soma de dois numeros
+# n1 = int(input('Digite um numero: '))
+# n2 = int(input('Digite outro numero: '))
+# s = n1+n2
+# print('A soma dos dois numeros é: ', s)
+
+# ///////////////////////////////////////////////////////
+
+# #programa que mostra o tipo primitivo
+# n1 = int(input('Digite um numero: '))
+# print(type(n1))
+# letra = input('Digite uma palavra: ')
+# print(type(letra))
+# teste = bool(input('Voce é um robo?: '))
+# print(type(teste))
+
+# ///////////////////////////////////////////////////////////
+
+#Antecessor e Sucessor
+# n1 = int(input('Digite um numero: '))
+# ant = n1 - 1
+# suc = n1 + 1
+# print('O antecessor é: ', ant, 'e o sucessor é: ', suc)
+
+#///////////////////////////////////////////////////////////////
+
+#Programa para calcular media
+# n1 = int(input('Digite a primeira nota:'))
+# n2 = int(input('Digite a segunda mota: '))
+# media = (n1 + n2)/2
+# print('A media das duas notas é: ', media)
+
+#/////////////////////////////////////////////////////////////////
+
+# Tabuada de um numero
+# n = int(input('Digite um numero: '))
+# print(f'{n} x {1} = {n*1}')
+# print(f'{n} x {2} = {n*2}')
+# print(f'{n} x {3} = {n*3}')
+# print(f'{n} x {4} = {n*4}')
+# print(f'{n} x {5} = {n*5}')
+# print(f'{n} x {6} = {n*6}')
+# print(f'{n} x {7} = {n*7}')
+# print(f'{n} x {8} = {n*8}')
+# print(f'{n} x {9} = {n*9}')
+# print(f'{n} x {10} = {n*10}')
+
+#//////////////////////////////////////////////////////////////////
+
+# Programa para desconto de 5%
+# n = int(input('Digite o preço do produto'))
+# desc = n*5/100
+# n2 = n - desc
+# print('O preço final do produto com o desconto de 5% é: ', n2)
+
+
+
