6da76beda02133ad5aef4bba85063c3c76401d8c
Kassia-Hikari <kassiahikari@outlook.com>
2025-03-25 21:42:28 -0400

    Add files via upload

---
+++ 12.py
@@ -0,0 +1,6 @@
+nome = input('Digite o nome de usuario ')
+senha = input('Digite a senha ')
+if nome == "kassia" and senha == "asdf":
+    print('Bem vindo')
+else:
+    print('Usuario ou senha invalidos')
