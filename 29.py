8a9580c374fa0dccb7067ae63449b082878ab73b
Kassia-Hikari <kassiahikari@outlook.com>
2025-06-13 14:47:55 -0400

    Add files via upload

---
+++ 29.py
@@ -0,0 +1,19 @@
+classe = []
+alunos = ("bia", "emilly", "joicy", "patrik", "vitor", "amabile")
+
+for aluno in alunos:
+    resposta = input(f"{aluno} Faltou ou está Presente?: ")
+    if resposta == "presente":
+        classe.append(aluno)
+
+print(f"Total de alunos presentes: {classe}")
+
+buscar = input("Deseja buscar o nome de um aluno? s ou n: ")
+if buscar == "s":
+    nome = input("Digite o nome do aluno: ")
+    if nome in classe:
+        print(f"{nome} estava presente.")
+    else:
+        print(f"{nome} faltou.")
+else:
+    print(f"total de alunos presentes {classe}")
